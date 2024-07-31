8d0a54d84278edd08f3dd803276f949acd53da3c
amirlevis <amir.levi.pro@gmail.com>
2024-07-31 14:10:39 +0300

    CHUCK_API

--- .github/actions/read_cn_joke/read_cn_joke.py
+++ .github/actions/read_cn_joke/read_cn_joke.py
@@ -26,7 +26,7 @@ class ChuckNorris(Base):
         if response.status_code == 200:
             data = response.json()
             joke = data.get("value", "No joke found.")
-            return joke
+            print(joke)
         else:
             raise Exception("Failed to retrieve a joke.")
 
